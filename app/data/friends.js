619410bf14b3485fe812e801f590b9700eed379b
Jeff <jeffschultz.js@gmail.com>
2017-07-21 13:10:10 -0700

    init files, set up most files to work, need js logic for friend matching

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendsArray = [
+    {
+        name: 'Bob Paulson',
+        photo: 'http://www.comicbookreligion.com/img/b/o/Bob_Paulson.jpg',
+        scores: [5, 4, 4, 3, 2, 1, 2, 5, 3, 1]
+    },
+    {
+        name: 'Tyler Durden',
+        photo: 'https://s-media-cache-ak0.pinimg.com/originals/a3/b2/7d/a3b27d91923b4955bcc86e7ae87dfcd3.jpg',
+        scores: [5, 3, 4, 1, 1, 3, 4, 5, 4, 3]
+    },
+    {
+        name: 'Angel Face',
+        photo: 'http://www.comicbookreligion.com/img/a/n/Angel_Face.jpg',
+        scores: [3, 2, 3, 1, 5, 4, 5, 3, 1, 3]
+    },
+    {
+        name: 'Marla Singer',
+        photo: 'https://s-media-cache-ak0.pinimg.com/originals/60/5f/4d/605f4d3232b0ca13f71e2811aeb2082a.jpg',
+        scores: [1, 2, 3, 5, 3, 1, 3, 2, 1, 5]
+    }
+];
+
+module.exports = friendsArray;
