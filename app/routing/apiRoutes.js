6b9e0356637410736f5bfa3762e60893e3dacda1
Jeff <jeffschultz.js@gmail.com>
2017-07-21 23:13:20 -0700

    finished js and post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,22 @@
 
 var friendsData = require("../data/friends");
 
+function compare(newFriend) {
+    var difference = [];
 
+    for (var i = 0; i < friendsData.length; i++) {
+        var val = 0
+        
+        for (var j = 0; j < newFriend.scores.length; j++) {
+            val += Math.abs(parseInt(newFriend.scores[j]) - parseInt(friendsData[i].scores[j]));
+        }
+        difference.push(val);
+        // console.log(val);
+    }
+
+    friendsData.push(newFriend);
+     return friendsData[difference.indexOf(Math.min(...difference))];
+}
 
 module.exports = function(app) {
 
@@ -12,8 +27,8 @@ module.exports = function(app) {
 
 
   app.post("/api/friends", function(req, res) {
-
-      friendsData.push(req.body);
+      var newFriend = req.body
+      res.send(compare(newFriend));
       res.json(true);
   });
 }
